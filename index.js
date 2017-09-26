8cce9afc4decc9476c2caee58b883926a53bc6bd
Tracy Fernandez <tr.ferna@gmail.com>
2017-09-26 18:26:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return `Happy Holidays, ${name}!`
 }
-function happyHolidayTo("holiday", "name") {
+
+function happyHolidayTo(holiday, name) {
   console.log("Happy Halloween, Susan!")
   return `Happy ${holiday}, ${name}`
 }
+
 function holidayCountdown("holiday", "days") {
   return `It's ${days} days until ${holiday}!`
 }
